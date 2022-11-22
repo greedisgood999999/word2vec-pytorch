512a5525b62c6c3ba225ea720cecb1fe3ed79d5f
Sergey Grudachev <59767329+greedisgood999999@users.noreply.github.com>
2022-11-22 18:02:31 +0600

    learn word2vec

--- train.py
+++ train.py
@@ -18,24 +18,22 @@ from utils.helper import (
 def train(config):
     os.makedirs(config["model_dir"])
     
-    train_dataloader, vocab = get_dataloader_and_vocab(
-        model_name=config["model_name"],
-        ds_name=config["dataset"],
-        ds_type="train",
-        data_dir=config["data_dir"],
-        batch_size=config["train_batch_size"],
-        shuffle=config["shuffle"],
-        vocab=None,
+    train_dataloader, vocab = get_dataloader_and_vocab(model_name=config["model_name"],
+                                                       ds_name=config["dataset"],
+                                                       ds_type="train",
+                                                       data_dir=config["data_dir"],
+                                                       batch_size=config["train_batch_size"],
+                                                       shuffle=config["shuffle"],
+                                                       vocab=None,
     )
 
-    val_dataloader, _ = get_dataloader_and_vocab(
-        model_name=config["model_name"],
-        ds_name=config["dataset"],
-        ds_type="valid",
-        data_dir=config["data_dir"],
-        batch_size=config["val_batch_size"],
-        shuffle=config["shuffle"],
-        vocab=vocab,
+    val_dataloader, _ = get_dataloader_and_vocab(model_name=config["model_name"],
+                                                 ds_name=config["dataset"],
+                                                 ds_type="valid",
+                                                 data_dir=config["data_dir"],
+                                                 batch_size=config["val_batch_size"],
+                                                 shuffle=config["shuffle"],
+                                                 vocab=vocab,
     )
 
     vocab_size = len(vocab.get_stoi())
